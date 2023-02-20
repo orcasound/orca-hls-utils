e0c7022a2e10127197d6dbcb9ecfd0b6f14252de
valentina <vms16@uw.edu>
2023-02-19 21:21:03 -0800

    lots of black formating

--- src/orca_hls_utils/HLSStream.py
+++ src/orca_hls_utils/HLSStream.py
@@ -1,34 +1,34 @@
-# HLSStream class 
-import urllib.request
-import os
-import m3u8
+# HLSStream class
 import math
-import ffmpeg #ffmpeg-python
-from . import scraper
-from datetime import datetime
-from datetime import timedelta
-
+import os
 import time
-from pytz import timezone
-from . import datetime_utils
+import urllib.request
+from datetime import datetime, timedelta
 from pathlib import Path
 
 import boto3
+import ffmpeg  # ffmpeg-python
+import m3u8
 from botocore import UNSIGNED
 from botocore.config import Config
+from pytz import timezone
+
+from . import datetime_utils, scraper
+
 
 def get_readable_clipname(hydrophone_id, cliptime_utc):
     # cliptime is of the form 2020-09-27T00/16/55.677242Z
-    cliptime_utc = timezone('UTC').localize(cliptime_utc)
-    date = cliptime_utc.astimezone(timezone('US/Pacific'))
-    date_format='%Y_%m_%d_%H_%M_%S_%Z'
+    cliptime_utc = timezone("UTC").localize(cliptime_utc)
+    date = cliptime_utc.astimezone(timezone("US/Pacific"))
+    date_format = "%Y_%m_%d_%H_%M_%S_%Z"
     clipname = date.strftime(date_format)
     return hydrophone_id + "_" + clipname, date
 
-#TODO (@prgogia) Handle errors due to rebooting of hydrophone
-class HLSStream():
+
+# TODO (@prgogia) Handle errors due to rebooting of hydrophone
+class HLSStream:
     """
-    stream_base = 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_orcasound_lab'
+    stream_base = 'https://s3-us-west-2.amazonaws.com/streaming-orcasound-net/rpi_orcasound_lab' # noqa
     polling_interval = 60 sec
     """
 
@@ -36,22 +36,26 @@ class HLSStream():
         self.stream_base = stream_base
         self.polling_interval = polling_interval
         self.wav_dir = wav_dir
-        bucket_folder = self.stream_base.split("https://s3-us-west-2.amazonaws.com/")[1]
+        bucket_folder = self.stream_base.split(
+            "https://s3-us-west-2.amazonaws.com/"
+        )[1]
         tokens = bucket_folder.split("/")
         self.s3_bucket = tokens[0]
         self.hydrophone_id = tokens[1]
 
-    # this function grabs audio from last_end_time to 
+    # this function grabs audio from last_end_time to
     def get_next_clip(self, current_clip_end_time):
 
         # if current time < current_clip_end_time, sleep for the difference
         now = datetime.utcnow()
 
-        # the extra 10 seconds to sleep is to download the last .ts segment properly
-        time_to_sleep = (current_clip_end_time-now).total_seconds() + 10
+        # the extra 10 seconds to sleep is to download the last .ts segment
+        # properly
+        time_to_sleep = (current_clip_end_time - now).total_seconds() + 10
 
         if time_to_sleep < 10:
-            # This implies that we took more than polling interval seconds to do our processing
+            # This implies that we took more than polling interval seconds to
+            #  do our processing.
             # Ideally, we should set current_clip_end_time = now at this point
             current_clip_end_time = now
             time.sleep(10)
@@ -62,48 +66,73 @@ class HLSStream():
             # the very first time, it's ~20 seconds
             time.sleep(time_to_sleep)
 
-        current_clip_start_time = current_clip_end_time - timedelta(0,60)
+        current_clip_start_time = current_clip_end_time - timedelta(0, 60)
         clip_start_time = current_clip_start_time.isoformat() + "Z"
-        clipname, _ = get_readable_clipname(self.hydrophone_id, current_clip_start_time)
+        clipname, _ = get_readable_clipname(
+            self.hydrophone_id, current_clip_start_time
+        )
 
         # get latest AWS bucket
         print("Listening to location {loc}".format(loc=self.stream_base))
         latest = f"{self.stream_base}/latest.txt"
-        stream_id = urllib.request.urlopen(
-            latest).read().decode("utf-8").replace("\n", "")
+        stream_id = (
+            urllib.request.urlopen(latest)
+            .read()
+            .decode("utf-8")
+            .replace("\n", "")
+        )
 
         # stream_url for the current AWS bucket
         stream_url = "{}/hls/{}/live.m3u8".format(
-            (self.stream_base), (stream_id))
-        
+            (self.stream_base), (stream_id)
+        )
+
         # check if m3u8 file exists
-        s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+        s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
         prefix = "{}/hls/{}/live.m3u8".format(self.hydrophone_id, stream_id)
-        objs = s3.list_objects_v2(Bucket=self.s3_bucket, Prefix=prefix, MaxKeys=1, Delimiter="/")
-        
+        objs = s3.list_objects_v2(
+            Bucket=self.s3_bucket, Prefix=prefix, MaxKeys=1, Delimiter="/"
+        )
+
         # if it does not, exit
         if objs["KeyCount"] == 0:
             print(".m3u8 file does not exist, will retry after some time")
             return None, None, current_clip_end_time
-        
+
         assert objs["KeyCount"] == 1
 
         # .m3u8 file exists so load it
         stream_obj = m3u8.load(stream_url)
         num_total_segments = len(stream_obj.segments)
-        num_segments_in_wav_duration = math.ceil(self.polling_interval/stream_obj.target_duration)
+        num_segments_in_wav_duration = math.ceil(
+            self.polling_interval / stream_obj.target_duration
+        )
 
-        # calculate the start index by computing the current time - start of current folder
-        current_clip_end_time_unix_pst = datetime_utils.get_unix_time_from_datetime_utc(current_clip_end_time)
-        time_since_folder_start = datetime_utils.get_difference_between_times_in_seconds(current_clip_end_time_unix_pst, stream_id)
+        # calculate the start index by computing the current time - start of
+        # current folder
+        current_clip_end_time_unix_pst = (
+            datetime_utils.get_unix_time_from_datetime_utc(
+                current_clip_end_time
+            )
+        )
+        time_since_folder_start = (
+            datetime_utils.get_difference_between_times_in_seconds(
+                current_clip_end_time_unix_pst, stream_id
+            )
+        )
         if time_since_folder_start < self.polling_interval + 20:
-            # This implies that possibly a new folder was created 
-            # and we do not have enough data for a 1 minute clip + 20 second buffer 
+            # This implies that possibly a new folder was created
+            # and we do not have enough data for a 1 minute clip + 20 second
+            # buffer
             # we exit and try again after hls polling interval
             None, None, current_clip_end_time
-        
-        min_num_total_segments_required = math.ceil(time_since_folder_start/stream_obj.target_duration)
-        segment_start_index = min_num_total_segments_required - num_segments_in_wav_duration + 1
+
+        min_num_total_segments_required = math.ceil(
+            time_since_folder_start / stream_obj.target_duration
+        )
+        segment_start_index = (
+            min_num_total_segments_required - num_segments_in_wav_duration + 1
+        )
         segment_end_index = segment_start_index + num_segments_in_wav_duration
 
         if segment_end_index > num_total_segments:
@@ -111,7 +140,7 @@ class HLSStream():
 
         # Create tmp path to hold .ts segments
         tmp_path = "tmp_path"
-        os.makedirs(tmp_path,exist_ok=True)
+        os.makedirs(tmp_path, exist_ok=True)
 
         file_names = []
         for i in range(segment_start_index, segment_end_index):
@@ -120,26 +149,28 @@ class HLSStream():
             file_name = audio_segment.uri
             audio_url = base_path + file_name
             try:
-                scraper.download_from_url(audio_url,tmp_path)
+                scraper.download_from_url(audio_url, tmp_path)
                 file_names.append(file_name)
             except Exception:
-                print("Skipping",audio_url,": error.")
+                print("Skipping", audio_url, ": error.")
 
         # concatentate all .ts files with ffmpeg
-        hls_file = (clipname+".ts")
-        audio_file = (clipname+".wav")
+        hls_file = clipname + ".ts"
+        audio_file = clipname + ".wav"
         wav_file_path = os.path.join(self.wav_dir, audio_file)
         filenames_str = " ".join(file_names)
-        concat_ts_cmd = "cd {tp} && cat {fstr} > {hls_file}".format(tp=tmp_path, fstr=filenames_str, hls_file=hls_file)
+        concat_ts_cmd = "cd {tp} && cat {fstr} > {hls_file}".format(
+            tp=tmp_path, fstr=filenames_str, hls_file=hls_file
+        )
         os.system(concat_ts_cmd)
-        
+
         # read the concatenated .ts and write to wav
         stream = ffmpeg.input(os.path.join(tmp_path, Path(hls_file)))
         stream = ffmpeg.output(stream, wav_file_path)
         ffmpeg.run(stream, quiet=True)
 
         # clear the tmp_path
-        os.system(f'rm -rf {tmp_path}')
+        os.system(f"rm -rf {tmp_path}")
 
         return wav_file_path, clip_start_time, current_clip_end_time
 
