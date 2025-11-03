318736a31ee1f35a700a62e08c76d156d7a8bde3
Dave Thaler <dthaler1968@gmail.com>
2025-11-03 11:48:15 -0800

    Remove trailing whitespace
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- tests/test_hlsstream.py
+++ tests/test_hlsstream.py
@@ -149,7 +149,7 @@ def test_invalid_nonexistent_bucket():
     )
     stream = HLSStream(stream_base, polling_interval, wav_dir)
     current_clip_end_time = datetime.utcnow() - timedelta(minutes=5)
-    
+
     # Expected to raise HTTPError (301 redirect or 404) for non-existent bucket
     with pytest.raises(Exception):  # HTTPError expected
         wav_path, clip_start, clip_end = stream.get_next_clip(
@@ -185,7 +185,7 @@ def test_invalid_hydrophone_id():
     )
     stream = HLSStream(stream_base, polling_interval, wav_dir)
     current_clip_end_time = datetime.utcnow() - timedelta(minutes=5)
-    
+
     # Expected to raise HTTPError (404) for invalid hydrophone
     with pytest.raises(Exception):  # HTTPError expected
         wav_path, clip_start, clip_end = stream.get_next_clip(
